5c3c52caa729578da48356a677cafe2d062cd80b
Ishan-Agrawal1 <agrawalishan47@gmail.com>
2026-05-03 11:06:44 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,15 +6,6 @@ export default defineConfig({
   build: {
     outDir: "dist",
     sourcemap: false,
-    minify: "terser",
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          router: ["react-router-dom"],
-        },
-      },
-    },
   },
   server: {
     proxy: {
